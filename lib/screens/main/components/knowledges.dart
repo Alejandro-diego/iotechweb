2d2ce324ae769e0e1fb3dfb99d711888d8e62e15
alemax75 <ale_max2015@yahoo.com.br>
2023-05-27 14:08:17 -0300

    second commit

---
+++ lib/screens/main/components/knowledges.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../constants.dart';
+
+class Knowledges extends StatelessWidget {
+  const Knowledges({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+      const  Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+              vertical: defaultPadding),
+          child: Text(
+            "Conhecimentos",
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+        ),
+        const KnowledgeText(text: "Flutter, Dart"),
+        const KnowledgeText(text: "Xocde, Swift, IOs"),
+        const KnowledgeText(text: "Iot, Automação, Electrónica"),
+        const KnowledgeText(text: "Atmega , ESP32 , ESP8266"),
+        const KnowledgeText(text: "Arduino Ide , C++ "),
+      ],
+    );
+  }
+}
+
+class KnowledgeText extends StatelessWidget {
+  const KnowledgeText({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: defaultPadding / 2),
+      child: Row(
+        children: [
+          SvgPicture.asset("assets/icons/check.svg"),
+          const SizedBox(width: defaultPadding / 2),
+          Text(text),
+        ],
+      ),
+    );
+  }
+}
