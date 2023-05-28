96db931d93b0720eec2ae2141e2534fbdb174898
alemax75 <ale_max2015@yahoo.com.br>
2023-05-28 17:37:11 -0300

    first commit

--- lib/models/Project.dart
+++ lib/models/Project.dart
@@ -31,18 +31,24 @@ List<Project> demoprojects = [
       
   ),
   Project(
-    title: "Chat/Messaging App Light and Dark Theme - Flutter UI",
+    title: "Stock Control Light e Dark Theme - Flutter UI",
     description:
-        "Today we gonna build messing/chat app #ui using #flutter that runs both Android and iOS devices also has a dark and light theme. We create in total 4 screens all of that support both Dark Theme and Light Theme. At first, we design a welcome screen that contains an image with a tag line also has a skip button.",
+        "that contains an image with a tag line also has a skip button.",
+        refTag: 'stock',
+        imageAsstes: 'assets/images/stock.png'
   ),
   Project(
     title: "Welcome page, Login Page and Sign up page - Flutter UI",
     description:
-        "In the first part of our complete e-commerce app, we show you how you can create a nice clean onboarding screen for your e-commerce app that can run both Andriod and iOS devices because it builds with flutter. Then on the second episode, we build a Sign in, Forgot Password screen with a custom error indicator.",
+        "Inisode, we build a Sign in, Forgot Password screen with a custom error indicator.",
+        refTag: 'spa',
+         imageAsstes: 'assets/images/spa.png'
   ),
   Project(
     title: "Covid-19 App - Flutter UI",
     description:
-        "We redesign the outlook app also make it responsive so that you can run it everywhere on your phone, tab, or web. In this flutter responsive video, we will show you the real power of flutter. Make mobile, web, and desktop app from a single codebase.",
+        "er. Make mobile, web, and desktop app from a single codebase.",
+        refTag: 'rele',
+        imageAsstes: 'assets/images/relecontrol.png'
   ),
 ];
